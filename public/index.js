8230bec1ce0f5e5238d50d96e19810f4f74dbd2f
olga <olga.i.rubleva@gmail.com>
2021-05-02 14:32:08 -0700

    finished form html/css

---
+++ public/index.js
@@ -0,0 +1,16 @@
+const btnCreateAccount = document.querySelector('.btn-create');
+const footer = document.querySelector('.signup__footer');
+const btnSignUp = document.querySelector('.btn-signup');
+const regBtns = document.querySelector('.registration-btns');
+
+console.log(regBtns)
+
+btnCreateAccount.addEventListener('click', showCreateAccountForm);
+
+function showCreateAccountForm(){
+    btnSignUp.style.display = 'none';
+    footer.classList.add('show');
+    regBtns.style.display = 'block';
+
+
+}
